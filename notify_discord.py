32f794b4e649cd1cc64fa613f1d4ff23144418a5
White-On <74248238+White-On@users.noreply.github.com>
2026-04-15 21:44:20 +0200

    change message

--- notify_discord.py
+++ notify_discord.py
@@ -19,10 +19,19 @@ async def main():
         
         notification_message = (
         "Hello! This is a notification to tell you that"
-        "I've been updated with new features!"
-        "Feel free to check them out and let me know what you think!"
+        "I've been updated with new features!\n"
+        "You can go check out over here: https://github.com/White-On/Discord_bot"
+        "Here is a quick recap how to fetch the latest version of the bot and launch it:\n" 
+        "1. Pull the latest version of the code from the GitHub repository.\n" 
+        "```git pull```\n"
+        "2. Create a virtual environment and activate it.\n"
+        "3. Install the required dependencies\n"
+        "```pip install uv```\n"
+        "```uv sync```\n"
+        "4. Run the bot\n"
+        "```uv run main.py```\n"
         )
-        
+
         await user.send(notification_message)
         print(f"Notification sent to {user.id}")
     await client.close()
